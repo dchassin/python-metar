285903b09b3a050adbd2313689300e1cb19ec2a0
David P. Chassin <david.chassin@me.com>
2021-07-11 12:41:06 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,8 @@ pprint = PrettyPrinter(indent=4).pprint
 
 ucar_weather.debug = True
 
-pprint(ucar_weather.stations())
-pprint(ucar_weather.get("KSFO"))
+station = "KSFO"
+stations = ucar_weather.stations()
+
+pprint(stations[station])
+pprint(ucar_weather.get(station))
