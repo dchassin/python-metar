d5d6f71e7559570e6654efd774c640def48f0eca
David P. Chassin <david.chassin@me.com>
2021-07-11 12:23:35 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,7 @@ import ucar_weather
 from pprint import PrettyPrinter
 pprint = PrettyPrinter(indent=4).pprint
 
+ucar_weather.debug = True
+
+pprint(ucar_weather.stations())
 pprint(ucar_weather.get("KSFO"))
